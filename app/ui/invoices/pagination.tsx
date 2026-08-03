120168524f0b915b7c4f727041b1da604e384d0d
Coder2100 <mtotsoz@gmail.com>
2026-08-03 18:05:36 +0200

    search and pagination functionality

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -5,7 +5,29 @@ import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
 
+import { usePathname, useSearchParams } from 'next/navigation';
+
 export default function Pagination({ totalPages }: { totalPages: number }) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get('page')) || 1;
+
+const createPageURL = (pageNumber: number | string ) => {
+    const params = new URLSearchParams(searchParams);
+
+    if (typeof pageNumber === 'number') {
+      params.set('page', pageNumber.toString());
+    } 
+    return `${pathname}?${params.toString()}`;
+
+}
+
+
+
+
+
+
+
   // NOTE: Uncomment this code in Chapter 10
 
   // const allPages = generatePagination(currentPage, totalPages);
