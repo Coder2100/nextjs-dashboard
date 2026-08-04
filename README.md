ae6cff11c8bf8f13fb447dfb23bc7bde923140b4
Coder2100 <mtotsoz@gmail.com>
2026-08-04 23:24:13 +0200

    Adding authentication

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ## Next.js App Router Course - Starter
 
 This is the starter template for the Next.js App Router Course. It contains the starting code for the dashboard application.
-
+Log in and out of your application using the following credentials
+Email: user@nextmail.com
+Password: 123456
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,9 @@ import {revalidatePath} from 'next/cache';
 import postgres from 'postgres';
 import { redirect } from 'next/navigation';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 const allowSsl = process.env.POSTGRES_SSL === 'true';
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: allowSsl ? { rejectUnauthorized: false } : false });
 
@@ -114,5 +117,22 @@ export async function deleteInvoice(id: string) {
     revalidatePath('/dashboard/invoices');
 }
 
-
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
  
